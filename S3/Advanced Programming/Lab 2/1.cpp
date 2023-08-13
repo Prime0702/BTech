d54137ce1ddc4e0aebb95c0112079429d55d4a10
AadithyanRaju <aadithyan75@gmail.com>
2023-08-13 16:33:23 +0530

    commit

---
+++ S3/Advanced Programming/Lab 2/1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+using namespace std;
+int main(){
+    priority_queue<int,vector<int>,std::less<int>> cstk;
+
+    cstk.push(1000);
+    cstk.push(100);
+    cstk.push(30);
+    cstk.push(400);
+    cstk.push(500);
+
+    while (!cstk.empty())
+    {
+        cout << cstk.top() << endl;
+        cstk.pop();
+    }
+    
+}
