b24e5a48d9e36ec0db86905db1979f2cba722619
Aadithyan Raju <aadithyan75@gmail.com>
2023-11-01 09:53:27 +0530

    commit

--- S5/Design and Analysis of Algorithms/LAB/lab 2/02 - Selection Sort.py
+++ S5/Design and Analysis of Algorithms/LAB/lab 2/02 - Selection Sort.py
@@ -6,3 +6,17 @@ def selectionSort(arr:list)->list:
                 min = j
         arr[i],arr[min]=arr[min],arr[i]
     return arr
+
+def selectionSortBestCase(arr:list)->list:
+    flag = True
+    for i in range(len(arr)):
+        flag = False
+        min = i
+        for j in range(i+1,len(arr)):
+            if arr[min]>arr[j]:
+                min = j
+                flag = True
+        arr[i],arr[min]=arr[min],arr[i]
+        if flag == False:
+            break
+    return arr
