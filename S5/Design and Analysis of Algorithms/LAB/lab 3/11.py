dcdd5db35586af5c1f3dd6c1be2f83a8e44e86e4
Aadithyan Raju <aadithyan75@gmail.com>
2023-12-27 09:43:38 +0530

    commit

--- S5/Design and Analysis of Algorithms/LAB/lab 3/11.py
+++ S5/Design and Analysis of Algorithms/LAB/lab 3/11.py
@@ -1,11 +1,6 @@
 def count_ways(score):
-    # Create a list to store the number of ways to reach each score
     ways = [0] * (score + 1)
-
-    # Base cases
     ways[0] = 1
-
-    # Iterate through all possible scores
     for i in range(3, score + 1):
         ways[i] += ways[i - 3]
     for i in range(5, score + 1):
@@ -15,7 +10,6 @@ def count_ways(score):
 
     return ways[score]
 
-# Test the function
 score = 15
 num_ways = count_ways(score)
 print(f"The number of ways to reach a score of {score} is {num_ways}.")
